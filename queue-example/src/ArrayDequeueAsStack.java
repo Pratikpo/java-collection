6137025c7c785dfbed446e61941b44bcb09c6020
pratik powar <pratik9885@gmail.com>
2023-09-12 22:18:47 +0530

    Dequeue updated

--- queue-example/src/ArrayDequeueAsStack.java
+++ queue-example/src/ArrayDequeueAsStack.java
@@ -29,7 +29,7 @@ public class ArrayDequeueAsStack {
         Integer.pop();
         print(Integer);
         Integer.push(50);
-        Integer.push(50);
+
 
         Integer.push(60);
         print(Integer);
