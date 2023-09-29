5c244d2facc5fb9526174ad6cd7ec2fb37127793
pratik powar <pratik9885@gmail.com>
2023-09-30 01:01:47 +0530

    map example

--- map-example/src/TreeMapExample.java
+++ map-example/src/TreeMapExample.java
@@ -1,10 +1,11 @@
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
-public class LinkedHashMapExample {
+public class TreeMapExample {
     public static void main(String[] args) {
-        Map<Long,String> myPhonebook= new LinkedHashMap<>();
+        Map<Long,String> myPhonebook= new TreeMap<>();
         myPhonebook.put(9552930270L,"pratik");
         myPhonebook.put(95342930270L,"kedar");
         myPhonebook.put(9554930270L,"karan");
